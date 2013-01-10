0216e6eaed90cc4242a3ed1551e8bb2bc2277f44
Steve Henry <spurioso@gmail.com>
2013-01-10 17:25:08 -0500

    eek

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Dhwi::Application.routes.draw do
+  get "pages/about"
+  get "pages/index"
+
   resources :votes
 
 
@@ -54,7 +57,7 @@ Dhwi::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'topics#index'
+  root :to => 'pages#index'
 
   # See how all your routes lay out with "rake routes"
 
